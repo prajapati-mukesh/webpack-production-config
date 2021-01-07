9f8045a2b2b4d8d213b0fb688914fff9864d63a9
Mukesh Prajapati <mukesh.easternts@gmail.com>
2021-01-07 09:44:53 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+console.log(process.env.NODE_ENV);// To Console App's Current Mode
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+// @ts-nocheck
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+});
